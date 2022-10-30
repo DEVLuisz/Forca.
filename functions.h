1998e98fc27d8c988d718fe9dfa42c740ea4e665
Luis/ <82966665+DEVLuisz@users.noreply.github.com>
2022-10-30 16:56:29 -0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,14 @@
+//
+// Created by Void on 10/30/2022.
+//
+#define WORD_SIZE 20
+
+void logo();
+void chuts();
+int jaChutou(char letra);
+void drawGallows();
+void chooseWord();
+int win();
+int hanged();
+void addWord();
+int chutesErrados();
